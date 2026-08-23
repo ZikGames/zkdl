7eca841ebfad6eace4b52a96db04a2dc0d4421a3
Zik1213 <zik1213@outlook.com>
2026-08-23 23:43:07 +0300

    removed dirt slur

--- www/zkdl.su/js/main.js
+++ www/zkdl.su/js/main.js
@@ -7,15 +7,16 @@ var num_2 = 52;
 var dedpi = true;
 
 // console.log(dedpi + (num_2 - num_1));
-if ((num_2 - num_1) == 47 && dedpi) {
-  console.log("дедпи47хехехе")
-} else
-{
-  console.log("bruh")
+if (num_2 - num_1 == 47 && dedpi) {
+  console.log("дедпи47хехехе");
+} else {
+  console.log("bruh");
 }
 
 var matrix = [
-  ["дабудидабудайдидидидабудай", "ёжик", "крош"], [534, 546, 6], ["meow", 756], ["секс", "порнушка"]
+  ["дабудидабудайдидидидабудай", "ёжик", "крош"],
+  [534, 546, 6],
+  ["meow", 756],
 ];
-matrix[2] [2]
-console.log(matrix)
+matrix[2][2];
+console.log(matrix);
